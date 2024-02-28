927954b03a126246e61d2f125176c124b931c472
Navdeep Singh <navsingh31@WKMZT7DBEB29.global.publicisgroupe.net>
2024-02-28 16:24:17 +0530

    Initial Commit

--- packages/my-own-web/src/generators/preset/generator.ts
+++ packages/my-own-web/src/generators/preset/generator.ts
@@ -1,8 +1,10 @@
 import {
+  addDependenciesToPackageJson,
   addProjectConfiguration,
   formatFiles,
   generateFiles,
   Tree,
+  updateJson,
 } from '@nx/devkit';
 import * as path from 'path';
 import { PresetGeneratorSchema } from './schema';
@@ -11,15 +13,31 @@ export async function presetGenerator(
   tree: Tree,
   options: PresetGeneratorSchema
 ) {
-  const projectRoot = `libs/${options.name}`;
+  const projectRoot = `.`;
   addProjectConfiguration(tree, options.name, {
     root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
+    projectType: 'application',
     targets: {},
   });
+
+  updateJson(tree, 'package.json', (json) => {
+    json.scripts = json.scripts || {};
+
+    json.scripts.start = 'vite build && vite preview';
+    return json;
+  });
+
   generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
   await formatFiles(tree);
+
+  return addDependenciesToPackageJson(
+    tree,
+    {},
+    {
+      vite: '^5.1.4',
+      typescript: '^5.2.2',
+    }
+  );
 }
 
 export default presetGenerator;
